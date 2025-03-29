cc787fd74cea790be2d84584a35bb74388186dba
Kacper Lukaszewski <251181@edu.p.lodz.pl>
2025-03-29 22:32:23 +0100

    9. Zastosowano możliwie najmniejszą widoczność dla atrybutów klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -7,7 +7,7 @@ public class DefaultCountingOutRhymer {
 
     private final int[] numbers = new int[MAX_SIZE];
 
-    public int total = -1;
+    private int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
